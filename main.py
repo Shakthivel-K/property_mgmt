ae7889e21a587a3541ac5159c85a589f4e850163
shakthivel <k.shakthivel2013@gmail.com>
2021-12-15 21:51:22 +0530

    basic functions have been tested for python functions to invoke sql queries

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import mysql.connector
+from mysql.connector import Error
 class connections:
     def create_connection(self,host_name,port_name,user_name,user_password,db_name):
-        connection=None
+        self.connection=None
+        self.host_name=host_name
+        self.port_name=port_name
+        self.user_name=user_name
+        self.user_password=user_password
+        self.db_name=db_name
         try:
-            connection = mysql.connector.connect(
+            self.connection = mysql.connector.connect(
                 host=host_name,
                 port=port_name,
                 user=user_name,
@@ -12,39 +19,21 @@ class connections:
             print("Connection to mysql successful")
         except Error as e:
             print("The error '{}' occurred".format(e))
-
-
-#################33
-import mysql.connector
-from mysql.connector import Error
-def create_connection(host_name,port_name,user_name,user_password,db_name):
-    connection=None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            port=port_name,
-            user=user_name,
-            database=db_name,
-            passwd=user_password
-        )
-        print("Connection to mysql successful")
-    except Error as e:
-        print("The error '{}' occurred".format(e))
-    return connection
-connection =create_connection("localhost","3306","admin","password","sand")
-def insertion_query(connection,database_query):
-    cursor=connection.cursor()
+root_connection=connections();
+root_connection.create_connection("localhost","3306","admin","password","sand")
+def insertion_query(connector,database_query):
+    cursor=connector.connection.cursor()
     result = None
     try:
         cursor.execute(database_query)
         result=cursor.fetchall()
-        connection.commit()
+        connector.connection.commit()
         return result
     except Error as e:
         print("The error '{}' occurred".format(e))
         return "error"
-def selection_query(connection,database_query):
-    cursor=connection.cursor()
+def selection_query(connector,database_query):
+    cursor=connector.connection.cursor()
     result =None
     try:
         cursor.execute(database_query)      
@@ -53,5 +42,10 @@ def selection_query(connection,database_query):
     except Error as e:
         print("The error '{}' occurred".format(e))
         return "error"
-create_users = "select * from tab;"
-print(selection_query(connection, create_users));
+command = "select * from tab;"
+print(selection_query(root_connection, command))
+command ="desc tab;"
+print (selection_query(root_connection,command))
+tups=selection_query(root_connection, command)
+
+
