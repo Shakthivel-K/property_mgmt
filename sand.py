1fe68f87fe767437b938f2f3f135ebdd0517200c
shakthivel <k.shakthivel2013@gmail.com>
2021-12-17 15:52:13 +0530

    tested two files with flask

--- sand.py
+++ sand.py
@@ -7,6 +7,10 @@ app = Flask(__name__,template_folder='html_files')
 def index():
     string=query_buffer.execute("tab","selection","select * from tab;",root_connection);
     return render_template("index.html",data=string)
+@app.route("/index2")
+def index2():
+    string=query_buffer.execute("tab","selection","select * from tab;",root_connection);
+    return render_template("index2.html",data=string)
 
 if __name__ == "__main__":
     app.run()
