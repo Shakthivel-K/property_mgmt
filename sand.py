85ba1deaab8ef66e4486fd996a6bce6e6febf379
shakthivel <k.shakthivel2013@gmail.com>
2021-12-17 16:11:13 +0530

    added basic_form.html to illustrate using forms in flask

--- sand.py
+++ sand.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from flask import render_template
 from main import *
 app = Flask(__name__,template_folder='html_files')
@@ -11,6 +11,13 @@ def index():
 def index2():
     string=query_buffer.execute("tab","selection","select * from tab;",root_connection);
     return render_template("index2.html",data=string)
+@app.route("/forms", methods =["GET", "POST"])
+def form_parse():
+    if (request.method=="POST"):
+        first_name = request.form.get("fname")
+        return render_template("basic_form.html",your_name=first_name)
+    return render_template("basic_form.html")
+    
 
 if __name__ == "__main__":
     app.run()
