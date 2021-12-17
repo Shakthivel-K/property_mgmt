b3d2c35e62923e7050839e1cac9859c9c0d7d977
shakthivel <k.shakthivel2013@gmail.com>
2021-12-17 09:50:21 +0530

    intial deployment of a simple flask app is a success

--- sand.py
+++ sand.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import render_template
+from main import *
 app = Flask(__name__,template_folder='html_files')
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    string=query_buffer.execute("tab","selection","select * from tab;",root_connection);
+    return render_template("index.html",data=string)
 
 if __name__ == "__main__":
     app.run()
