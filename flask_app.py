76f833dfa7b7f407b5778a550c097c4ece8c35a6
shakthivel <k.shakthivel2013@gmail.com>
2021-12-18 11:47:55 +0530

    added and edited flask apps

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from main import *
 
 app=Flask(__name__,template_folder="webpages")
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/sell")
+def sell_index():
+    return render_template("sell_index.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login_page():
+    if (request.method=="POST"):
+        receptionist=connections()
+        
+
 if __name__ == "__main__":
     app.run()
 
