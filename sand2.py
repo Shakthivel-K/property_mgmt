4e8c7db9982f00c45dcfdde995dfc38882025f7e
Shakthivel Kathiravan <k.shakthivel2013@gmail.com>
2021-12-19 21:52:46 +0530

    complicated sand2.py

--- sand2.py
+++ sand2.py
@@ -3,12 +3,26 @@ reader =open("datas.csv","r")
 writer=open ("commands.txt","a")
 enable_comma=1
 table_name=""
+ch=[]
 for row in reader:
-    print(row)
     cells=row.split(',')
     if (cells[0]=="$"):
         table_name=cells[1]
+        ch.clear()
+        for i in range(2,len(cells),1):
+            ch.append(cells[i].rstrip('\n'))
+        print(ch)
         
     else:
-        writer.write("insert into "+table_name.rstrip('\n')+" values ("+row.rstrip('\n')+");\n")
-        
+        #writer.write("insert into "+table_name.rstrip('\n')+" values ("+row.rstrip('\n')+");\n")
+        writer.write("insert into "+table_name.rstrip('\n')+" values (");
+        for i  in range(0,len(ch),1):
+            if ch[i]=="str":
+                writer.write("\'"+cells[i]+"\'")
+            else :
+                writer.write(cells[i])
+            if  i==(len(ch)-1):
+                    writer.write(");")
+            else:
+                writer.write(",")
+        writer.write("\n")
