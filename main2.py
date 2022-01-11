408a114737915d2ac0f966fad00a90405284aee0
shakthivel <k.shakthivel2013@gmail.com>
2022-01-11 23:39:21 +0530

    sync commit

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 def log_message(msg):
     file=open("message.txt")
     file.write(msg)
-class connections:
+class connector:
     def create_connection(self,host_name,port_name,user_name,user_password,db_name):
         self.connection=None
         self.host_name=host_name
@@ -24,43 +24,26 @@ class connections:
         except Error as e:
             print("The error '{}' occurred".format(e))
 class queries:
-    def execute(self,table_name,mode,database_query,connector):
-        self.table_name=table_name
+    def __init__(self,connectors,mode):
+        self.connectors=connectors
         self.mode=mode
-        self.database_query=database_query
-        cursor=connector.connection.cursor()
+        self.result=[]
+        self.cmd=""
+    def execute(self,cmd):
+        self.cmd=cmd
+        cursor=self.connectors.connection.cursor()
         self.result = None
         try:
-            cursor.execute(database_query)
+            cursor.execute(cmd)
             self.result=cursor.fetchall()
             connector.connection.commit()
         except Error as e:
             print("The error '{}' occurred".format(e))
             self.result="error"
-        if (mode=="insertion"):
-            cursor.commit();
-        return self.result    
-class results:
-    def __init__(self,header,query):
-        self.header=header
-        self.query=query  
-        self.degree=len(header)
-        self.cardinality=len(query.result)
+        if (self.mode=="insertion"):
+            cursor.commit()
+        return self.result  
 
-    def display_cmd(self):
-        for i in self.header:
-            print(i,end='\t')
-        print('\n')
-        for i in self.query.result:
-            print(i,end='\t')
-        print('\n')
-        ##write functions as per requirement
-
-root_connection=connections();
-root_connection.create_connection("localhost","3306","admin","password","sand")
-query_buffer=queries()
-query_buffer.execute("tab","selection","select * from coliving;",root_connection);
-#print (query_buffer.result)
 def parse_result(connector,query):
     if (query.result=="error"):
         return []
@@ -74,8 +57,4 @@ def parse_result(connector,query):
         for i in range(0,num_attributes,1):
             header+=table_data_query.result[i]
         res=results(header,query)
-        return res
-
-
-
-    
+        return res    
